64bdc1c5c1950a8468c12cf190cccc94e23e5eaa
unknown <carral21@gmail.com>
2015-11-15 00:15:59 +0100

    Pequeños retoques a la propuesta inicial

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 (function(){
-  "use strict";
+console.log("uy");
+var menuIcon = $('#menuimg');
+var menuImg = function(){
+  menuIcon.attr('src', "./images/" + this.id + ".png");
+};
+  $('#home').hover(menuImg);
+  $('#add').hover(menuImg);
+  $('#aleatorio').hover(menuImg);
+  $('#busqavz').hover(menuImg);
 
+$('#menu').each(function(){
+  $(this).mouseleave(function(){
+    menuIcon.attr('src', './images/menuicon.png');
+  });
+});
 })();
