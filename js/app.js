79887bf6cc22c57dbec225ad542e5df13bcc19f8
unknown <carral21@gmail.com>
2015-11-21 02:02:03 +0100

    Cambio total diseño

--- js/app.js
+++ js/app.js
@@ -1,17 +1,4 @@
+'use strict';
 (function(){
-console.log("uy");
-var menuIcon = $('#menuimg');
-var menuImg = function(){
-  menuIcon.attr('src', "./images/" + this.id + ".png");
-};
-  $('#home').hover(menuImg);
-  $('#add').hover(menuImg);
-  $('#aleatorio').hover(menuImg);
-  $('#busqavz').hover(menuImg);
 
-$('#menu').each(function(){
-  $(this).mouseleave(function(){
-    menuIcon.attr('src', './images/menuicon.png');
-  });
-});
 })();
