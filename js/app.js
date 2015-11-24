92ed3e46fb8839e3856422d2da5d8004fa371397
Joseba <jcarraldc@gmail.com>
2015-11-24 22:38:30 +0100

    Añadir y borrar de la lista de ingredientes, js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 'use strict';
 (function(){
 
+
+
+//Formulario
+
+/*
+* Introduce un ingrediente nuevo a la lista y activa el evento de borrar de la lista
+*/
+
+$(".label-newIngred").click(function(){
+  if($('#newCantidadgr').val().length == 0 || $('#newIngrediente').val().length == 0 )
+    alert('Introduce todos los campos');
+  else{
+    var list = "<li class='ingrediente' name='ingrediente'>" + $('#newIngrediente').val()  + "     -" + $('#newCantidadgr').val()  +"gr "+ "<span class='delete'>X</span></li>";
+    $("#lista-ingredientes").append(list);
+    $("li.ingrediente:last-child>.delete").click(function() {
+      $(this).parent().fadeOut(function(){
+        $(this).remove();
+      });
+    });
+}
+});
+
 })();
