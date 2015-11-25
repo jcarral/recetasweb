af5d175c0035c3baa675fe65ce2618f88c6ce704
Joseba <jcarraldc@gmail.com>
2015-11-25 14:26:14 +0100

    Añadir imagen al formulario

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(".label-newIngred").click(function(){
   if($('#newCantidadgr').val().length == 0 || $('#newIngrediente').val().length == 0 )
     alert('Introduce todos los campos');
   else{
-    var list = "<li class='ingrediente' name='ingrediente'>" + $('#newIngrediente').val()  + "     -" + $('#newCantidadgr').val()  +"gr "+ "<span class='delete'>X</span></li>";
+    var list = "<oli class='ingrediente' name='ingrediente[]'>" + $('#newIngrediente').val()  + "     -" + $('#newCantidadgr').val()  +"gr "+ "<span class='delete'>X</span></li>";
     $("#lista-ingredientes").append(list);
     $("li.ingrediente:last-child>.delete").click(function() {
       $(this).parent().fadeOut(function(){
